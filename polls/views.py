ebc30d55ce54aa8b4fb668458d3d646d0c59f2f3
Dimitri CORREIA <dimitri.correia@imt-atlantique.net>
2022-03-12 22:19:04 +0100

    new views - detail, res, vote

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world from polls")
+
+def detail(request, question_id):
+    return HttpResponse(f"Question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Results of {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Vote for {question_id}")
