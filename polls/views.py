830803ed7277562396a0061dcfc45d09d4725657
Dimitri CORREIA <dimitri.correia@imt-atlantique.net>
2022-03-12 23:00:01 +0100

    shortcut get object or 404

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -19,14 +19,18 @@ def index(request):
 
 
 def detail(request, question_id):
+    """
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404(f"Question {question_id} doesn't exist ;/")
+    """
+    question = get_object_or_404(Question, pk=question_id)
     context = {
         'question': question
     }
     return render(request, 'polls/detail.html', context)
+
 
 def results(request, question_id):
     return HttpResponse(f"Results of {question_id}")
