d85cfa86c926cc7722612fbafd40e3003c557f59
Dimitri CORREIA <dimitri.correia@imt-atlantique.net>
2022-03-14 19:53:26 +0100

    classes for views

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,25 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse 
 from django.db.models import F
+from django.views import generic
 
 from .models import Question, Choice
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/resutls.html'                
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
