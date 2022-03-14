8321cc198417f5fc04c5c7d220723984a767207b
Dimitri CORREIA <dimitri.correia@imt-atlantique.net>
2022-03-14 10:53:45 +0100

    Avoiding race conditions using F()

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse 
+from django.db.models import F
 
 from .models import Question, Choice
 
@@ -53,7 +54,7 @@ def vote(request, question_id):
         'error_message': "You didn't select a choice :/",
         })
     else:
-        selected_choice.votes +=1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(
             reverse('polls:results',
